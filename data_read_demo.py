650c11ef072b4b370bcb8bb51f5949935adef810
Bryan Lin <bryan.lin.1124@hotmail.com>
2022-03-22 14:14:39 +0800

    2021/03/22 add qrs detect demo files and ignore file

--- data_read_demo.py
+++ data_read_demo.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+from qrs_detect_demo import QRS_detection
 
 data = pd.read_excel("data.xlsx", sheet_name="0-sitting",
                      header=0, usecols="B:F")
 ecg = data[0].T.values
+
+qrs = QRS_detection(ecg, 250, 200)
+print(qrs)
